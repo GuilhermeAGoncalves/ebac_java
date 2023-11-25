0ec084348b9a340b3dfc17d3a15884e979b380e4
GuilhermeAGoncalves <guilhermegpop@gmail.com>
2023-11-25 19:14:16 -0300

    :heavy_plus_sign: Adicionando tarefa de wrapper

---
+++ tarefa_wrapper/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println(number);
+        Integer number1 = Integer.valueOf(number);
+        System.out.println(number1);
+    }
+}
